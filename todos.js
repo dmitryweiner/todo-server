816e7f55f046c1ef1794d2755f0439408a03a108
dmitryweiner <dmitry.weiner@gmail.com>
2021-03-31 13:38:17 +0700

    Add more conditions

--- todos.js
+++ todos.js
@@ -8,17 +8,8 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    // check if empty
-    if (req.body.title === undefined || typeof req.body.title !== 'string' || req.body.title.length === 0) {
-        const error = new Error('No title provided');
-        error.status = 400;
-        return next(error);
-    }
-
-    // check if exists
-    if (db.find({title: req.body.title}).value()) {
-        const error = new Error('This item already exists');
-        error.status = 400;
+    const error = validateTodo({title: req.body.title});
+    if (error) {
         return next(error);
     }
 
@@ -40,17 +31,34 @@ router.put('/:id', (req, res, next) => {
         return next(error);
     }
 
-    if (db.find({title: req.body.title}).value()) {
-        const error = new Error('This item already exists');
-        error.status = 400;
-        return next(error);
+    if (req.body.title !== undefined) {
+        const error = validateTodo({title: req.body.title});
+        if (error) {
+            return next(error);
+        }
+        item.title = "" + req.body.title;
     }
 
-    item.title = "" + req.body.title;
-    item.isChecked = !!req.body.isChecked; // forgive me
+    if (req.body.isChecked !== undefined) {
+        item.isChecked = !!req.body.isChecked; // forgive me
+    }
 
     db.find({id: item.id}).assign(item).write();
     res.json(item);
 });
+
+function validateTodo({title}) {
+    let error;
+    if (title === undefined || typeof title !== 'string' || title.length === 0) {
+        error = new Error('No title provided');
+        error.status = 400;
+    }
+
+    if (db.find({title}).value()) {
+        error = new Error('This item already exists');
+        error.status = 400;
+    }
+    return error;
+}
 
 module.exports = router;
